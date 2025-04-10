de17cf1665240007c7c792ecdbc5cadbe6e88ce1
whimsy <whimsy@sylph.box>
2025-04-10 01:59:58 -0500

    modified:   server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3112;
 
 // Serve all files in the current directory as static
 app.use(express.static(path.join(__dirname)));
